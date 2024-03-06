6c55795a415f6b9f6cfbcbbaf64990bb6280d35c
EmanueleDornelles <dornelles.mb@gmail.com>
2024-03-06 19:18:33 -0300

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "82ac046066687f7635175062531ab0b2",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
